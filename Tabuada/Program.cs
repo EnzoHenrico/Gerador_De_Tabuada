4cb67e3e145993c84e25334acce9e8d922b40b64
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-18 15:40:14 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿// Faça um programa que leia um valor inteiro positivo e imprima a sua tabuada.
+int number = 0, multiplier = 0;
+
+do
+{
+    Console.Write("Diga um número para gerar sua tabuada: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number <= 0)
+    {
+        Console.WriteLine("O multiplicador deve ser positivo.");
+    }
+}
+while (number <= 0);
+
+
+while (multiplier <= 10)
+{
+    Console.WriteLine($"{number} x {multiplier} = {number * multiplier}");
+    multiplier++;
+}
